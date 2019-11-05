4327dab28b06a0efec071eb4822bc916945399dc
Tonye Jack <jtonye@ymail.com>
2019-11-05 08:39:25 -0500

    Added new line.

--- sample/tests.py
+++ sample/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 from .models import Library
 
-class TestCloneMixing(TestCase):
+class TestCloneMixin(TestCase):
     def test_clone_with_custom_id(self):
         base = Library.objects.create(name='First library')
         clone = base.make_clone()
