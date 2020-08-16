6f9f33d99026de41b039c31ac540ccb39909b8fc
Tonye Jack <jtonye@ymail.com>
2020-08-16 12:11:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -103,6 +103,7 @@ setup(
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
     ],
     install_requires=install_requires,
     tests_require=['coverage'],
