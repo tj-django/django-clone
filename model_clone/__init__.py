3afc0392d4f67620cea0907b687fb077b4b32134
Tonye Jack <tonye@coherentpath.com>
2020-12-02 09:43:56 -0500

    Moved import of create_copy_of_instance.

--- model_clone/__init__.py
+++ model_clone/__init__.py
@@ -1,10 +1,8 @@
 from model_clone.mixins.clone import CloneMixin
-from model_clone.utils import create_copy_of_instance
 from model_clone.admin import CloneModelAdmin
 
 
 __all__ = [
     "CloneMixin",
-    "CloneModelAdmin",
-    "create_copy_of_instance",
+    "CloneModelAdmin"
 ]
