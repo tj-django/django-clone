b5ad7ffaf3b320355697ac7500412de46e248f4e
Lint Action <lint-action@django-clone.com>
2021-03-20 17:22:08 +0000

    Fix code style issues with Black

--- model_clone/mixins/clone.py
+++ model_clone/mixins/clone.py
@@ -210,7 +210,9 @@ class CloneMixin(object):
                     "Conflicting configuration.",
                     hint=(
                         'Please provide either "_clone_fields"'
-                        + ' or "_clone_excluded_fields" for model {}'.format(cls.__name__)
+                        + ' or "_clone_excluded_fields" for model {}'.format(
+                            cls.__name__
+                        )
                     ),
                     obj=cls,
                     id="{}.E002".format(ModelCloneConfig.name),
@@ -223,7 +225,9 @@ class CloneMixin(object):
                     "Conflicting configuration.",
                     hint=(
                         'Please provide either "_clone_m2m_fields"'
-                        + ' or "_clone_excluded_m2m_fields" for model {}'.format(cls.__name__)
+                        + ' or "_clone_excluded_m2m_fields" for model {}'.format(
+                            cls.__name__
+                        )
                     ),
                     obj=cls,
                     id="{}.E002".format(ModelCloneConfig.name),
@@ -369,7 +373,9 @@ class CloneMixin(object):
         for field in one_to_one_fields:
             rel_object = getattr(self, field.related_name, None)
             if rel_object:
-                if hasattr(rel_object, "make_clone") and callable(rel_object.make_clone):
+                if hasattr(rel_object, "make_clone") and callable(
+                    rel_object.make_clone
+                ):
                     rel_object.make_clone(
                         attrs={field.remote_field.name: duplicate}, sub_clone=True
                     )
@@ -383,7 +389,9 @@ class CloneMixin(object):
             items = []
             for item in getattr(self, field.related_name).all():
                 try:
-                    item_clone = item.make_clone(attrs={field.remote_field.name: duplicate})
+                    item_clone = item.make_clone(
+                        attrs={field.remote_field.name: duplicate}
+                    )
                 except IntegrityError:
                     item_clone = item.make_clone(
                         attrs={field.remote_field.name: duplicate}, sub_clone=True
