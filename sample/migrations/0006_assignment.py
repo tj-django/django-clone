9f7358c1ac8e064bf1ebcfda7705f107984e88f7
Lint Action <lint-action@django-clone.com>
2021-03-20 17:33:52 +0000

    Fix code style issues with Black

--- sample/migrations/0006_assignment.py
+++ sample/migrations/0006_assignment.py
@@ -48,14 +48,16 @@ class Migration(migrations.Migration):
                 ),
                 (
                     "location",
-                    models.CharField(max_length=25, default='', verbose_name="Location"),
+                    models.CharField(
+                        max_length=25, default="", verbose_name="Location"
+                    ),
                 ),
                 (
                     "driver_type",
                     models.CharField(
                         choices=[(1, "Commercial"), (2, "Residential")],
                         max_length=2,
-                        default='',
+                        default="",
                         verbose_name="Driver type",
                     ),
                 ),
@@ -64,7 +66,7 @@ class Migration(migrations.Migration):
                     models.CharField(
                         choices=[(1, "Large"), (2, "Small")],
                         max_length=2,
-                        default='',
+                        default="",
                         verbose_name="Car type",
                     ),
                 ),
@@ -87,9 +89,7 @@ class Migration(migrations.Migration):
                 ),
                 (
                     "description",
-                    models.TextField(
-                        blank=True, verbose_name="Assignment description"
-                    ),
+                    models.TextField(blank=True, verbose_name="Assignment description"),
                 ),
                 (
                     "applied_drivers",
