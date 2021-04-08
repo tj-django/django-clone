9ae328be5b7c3a6f6f2a5bf2827aab96aee6178d
Tonye Jack <jtonye@ymail.com>
2021-04-08 16:19:42 -0400

    Fixed formatting.

--- model_clone/tests/test_clone_mixin.py
+++ model_clone/tests/test_clone_mixin.py
@@ -69,7 +69,9 @@ class CloneMixinTestCase(TestCase):
         self.assertEqual(instance.name, name)
         self.assertEqual(clone.created_by, instance.created_by)
 
-        self.assertNotEqual(instance.created_at.timestamp(), clone.created_at.timestamp())
+        self.assertNotEqual(
+            instance.created_at.timestamp(), clone.created_at.timestamp()
+        )
 
     def test_cloning_without_explicit_clone_m2m_fields(self):
         author_1 = Author.objects.create(
