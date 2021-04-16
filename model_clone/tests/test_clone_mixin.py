327b195caeee04fa43f833c973a15e738228fb67
Lint Action <lint-action@django-clone.com>
2021-04-16 01:17:27 +0000

    Fix code style issues with Black

--- model_clone/tests/test_clone_mixin.py
+++ model_clone/tests/test_clone_mixin.py
@@ -35,7 +35,7 @@ class CloneMixinTestCase(TestCase):
 
         with self.assertRaises(ValidationError):
             instance.make_clone()
-    
+
     def test_cloning_an_unsaved_instance_is_invalid(self):
         instance = Book()
 
