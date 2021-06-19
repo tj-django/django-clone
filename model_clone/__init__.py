e1f9e3472a9f2434d948111a3723275e071aed6d
Tonye Jack <jtonye@ymail.com>
2021-06-19 17:35:51 -0400

    Upgraded v2.5.3 → v2.6.0 (#374)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- model_clone/__init__.py
+++ model_clone/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Tonye Jack"""
 __email__ = "jtonye@ymail.com"
-__version__ = "2.5.3"
+__version__ = "2.6.0"
 
 from model_clone.admin import CloneModelAdmin, CloneModelAdminMixin
 from model_clone.mixins.clone import CloneMixin
