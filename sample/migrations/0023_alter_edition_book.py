97289568b6315d2336f1a39e18c836f8f554b4d2
Lint Action <github-action[bot]@github.com>
2022-06-24 13:32:54 +0000

    Fix code style issues with Black

--- sample/migrations/0023_alter_edition_book.py
+++ sample/migrations/0023_alter_edition_book.py
@@ -7,13 +7,18 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('sample', '0022_ending_sentence'),
+        ("sample", "0022_ending_sentence"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='edition',
-            name='book',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='editions', to='sample.book'),
+            model_name="edition",
+            name="book",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="editions",
+                to="sample.book",
+            ),
         ),
     ]
