fadf742815aca52a4a3e7f29d40add83a5588b89
Tonye Jack <jtonye@ymail.com>
2022-10-28 21:00:58 -0600

    Updated the test to include django 4.1

--- setup.py
+++ setup.py
@@ -111,6 +111,7 @@ setup(
         "Framework :: Django :: 3.1",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
     ],
     install_requires=install_requires,
     tests_require=["coverage"],
