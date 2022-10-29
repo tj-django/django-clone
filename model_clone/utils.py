3db34bbc9cdbef0b78229c7e3d4308ce6ffed5f9
Tonye Jack <jtonye@ymail.com>
2022-10-28 21:11:14 -0600

    indent code.

--- model_clone/utils.py
+++ model_clone/utils.py
@@ -234,7 +234,10 @@ def get_unique_value(
     if using is not None:
         qs = model._default_manager.using(using).all()
 
-    if (sys.version_info[0], sys.version_info[1]) > (3, 6) and not qs.filter(**{fname: value}).exists():
+    if (
+        (sys.version_info[0], sys.version_info[1]) > (3, 6)
+        and not qs.filter(**{fname: value}).exists()
+    ):
         return value
 
     it = generate_value(value, suffix, transform, max_length, max_attempts)
