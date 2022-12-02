cc6ca7a1b000ccffe4262963ccc279e7898001bc
Tonye Jack <jtonye@ymail.com>
2022-12-01 21:51:04 -0700

    Update signals.py

--- sample/signals.py
+++ sample/signals.py
@@ -3,7 +3,12 @@ from django.utils import timezone
 
 from model_clone.signals import post_clone_save, pre_clone_save
 
-from .models import Edition
+from .models import Edition, Book
+
+
+@receiver(pre_clone_save, sender=Book)
+def update_name(sender, instance, **kwargs):
+    instance.name = f'{instance.name} pre-save'
 
 
 @receiver(pre_clone_save, sender=Edition)
