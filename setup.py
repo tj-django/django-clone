33909faee6444c54652736ff10d200fefd3a0489
Tonye Jack <jtonye@ymail.com>
2023-01-30 20:30:14 -0700

    Upgraded v5.2.0 → v5.3.0 (#758)
    * Bump version: 5.2.0 → 5.3.0
    
    * Upgraded from v5.2.0 → v5.3.0
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: jackton1 <jackton1@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ else:
 
 setup(
     name="django-clone",
-    version="5.2.0",
+    version="5.3.0",
     description="Create a clone of a django model instance.",
     python_requires=">=3.6",
     long_description=LONG_DESCRIPTION,
