6c0192776680b2362aab570eb0f0d29611a24ad9
Tonye Jack <jtonye@ymail.com>
2025-06-10 10:59:11 -0600

    Update __init__.py

--- model_clone/__init__.py
+++ model_clone/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Tonye Jack"""
 __email__ = "jtonye@ymail.com"
-__version__ = "5.4.0"
+__version__ = "5.4.1"
 
 from model_clone.admin import CloneModelAdmin, CloneModelAdminMixin
 from model_clone.mixin import CloneMixin
